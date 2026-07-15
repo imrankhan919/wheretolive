a8937b3ef0d8386cfc0a7c4fd2e1d7dcdec90e74
imrankhan919 <imrankhan919.ik@gmail.com>
2026-07-15 13:32:09 +0530

    models and seeding script added

---
+++ seed/seed_runner.py
@@ -0,0 +1,39 @@
+import csv
+from sqlmodel import Session, select
+from database import engine
+from models.city import City, CityMetric 
+
+print("Starting Seeding")
+
+def run_seed() :
+    print("Function Called")
+    with Session(engine) as session : 
+        with open("wheretolive\app\seed\cities_seed.csv") as f :
+            reader = csv.DictReader(f)
+            for row in reader :
+                stmt = select(City).where(City.name == row["name"])
+                city = session.exec(stmt).first()
+                if not city : 
+                    city = City(
+                        name = row["name"],
+                        state = row["state"],
+                        population = int(row["population"])
+                    )
+                    session.add(city) 
+                    session.commit()
+                    session.refresh(city)
+                metric = CityMetric(
+                    city_id = city.id ,
+                    year = int(row["year"]),
+                    cost_of_living = float(row["cost_index"]),
+                    air_quality_index = float(row["air_index"]),
+                    safety_score = float(row["safety_score"]),
+                    healthcare_score = float(row["health_score"])
+                )
+                session.add(metric)
+            session.commit()
+        print("Seed Complete")
+        
+
+if __name__ == "__main__" : 
+    run_seed()
