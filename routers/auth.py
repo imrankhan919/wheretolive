fb288f96469c31c913db1755a20a868e7975231b
imrankhan919 <imrankhan919.ik@gmail.com>
2026-07-17 13:25:06 +0530

    user registration added

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , Depends, HTTPException
+from sqlmodel import Session , select
+from database import get_session
+from models.user import User
+from services.auth_service import hash_password
 
 router = APIRouter(prefix="/auth" , tags=["Auth"])
 
@@ -8,7 +12,13 @@ def login() :
     return {"message" : "User Logged In"}
 
 @router.post("/register") 
-def register() :
+def register(name : str , email : str , password : str , session : Session = Depends(get_session)) :
+    existing = session.exec(select(User).where(User.email == email)).first()
+    if existing : 
+        raise HTTPException(status_code=400 , detail="User Already Exists")
+    user = User(name = name , email = email , password=hash_password(password))
+    session.add(user)
+    session.commit()
     return {"message" : "User Registred"}
 
 @router.get("/profile") 
---
+++ services/auth_service.py
@@ -0,0 +1,11 @@
+import bcrypt
+
+
+def hash_password(password : str) :
+    password_bytes = password.encode('utf-8')[:72]
+    hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed.decode('utf-8')
+
+# def verify_password(plain_password: str, hashed_password: str) -> bool:
+#     password_bytes = plain_password.encode('utf-8')[:72]
+#     return bcrypt.checkpw(password_bytes, hashed_password.encode('utf-8'))
